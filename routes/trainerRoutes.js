b868826c97b140f806facb16f8b1655fa1dbf55d
vinayak <vin.k.rt24@dypatil.edu>
2025-10-27 23:56:45 +0530

    Simplify trainer routes and align with RESTful structure

--- routes/trainerRoutes.js
+++ routes/trainerRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
 const {
   getTrainers,
   addTrainer,
   deleteTrainer
 } = require('../controllers/trainerController');
 
+// GET all trainers
 router.get('/', getTrainers);
-router.post('/add', addTrainer);
+
+// POST add new trainer
+router.post('/', addTrainer);
+
+// DELETE trainer by ID
 router.delete('/:id', deleteTrainer);
 
 module.exports = router;
