09ca8dcf12974b452d8600a93cb62c2118ffc21c
vinayak <vin.k.rt24@dypatil.edu>
2025-10-28 00:45:35 +0530

    Remove email field and unique index from Trainer schema

--- models/Trainer.js
+++ models/Trainer.js
@@ -1,26 +1,11 @@
 const mongoose = require('mongoose');
 
 const trainerSchema = new mongoose.Schema({
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  timing: {
-    type: String,
-    required: true,
-  },
-  employment_status: {
-    type: String,
-    required: true,
-  }
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  age: { type: Number, required: true },
+  timing: { type: String, required: true },
+  employment_status: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Trainer', trainerSchema);
